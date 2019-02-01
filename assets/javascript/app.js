37c2f7fe75b4443b6796dc022aec3b76cd3bb22f
Clifton Noble <cliftonnoble@cliftons-mbp.lan>
2019-02-01 11:00:13 -0800

    adding fixes to js and index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-  // Initialize Firebase
+$(document).ready(function () {
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyBFtHqDL1eaHBo4otOz0TCfSIIKzhUTovw",
     authDomain: "train-scheduler-dd314.firebaseapp.com",
@@ -9,15 +10,26 @@
   };
   firebase.initializeApp(config);
 
-  //Global Variables
+  //form Variables (user input)
   let trainName = "";
   let destination = "";
   let firstTrainTime = "";
   let frequency = "";
+  //Time Conversion
+  let currentTime = moment().format("hh:mm");
+  let firstTimeConverted = "";
+  let diffTime = "";
+  let tRemainder;
+  let tMinutesTillTrain;
+  let nextTrain;
+  let nextTrainFormat;
+  //Database Variable
+  let database = firebase.database();
 
-  $("#run-search").on("click", function(event) {
-    //   $("#form-group").reset();
-      console.log("Working!")
+  $("#current-time").text(currentTime);
+  console.log(currentTime);
+  
+  $("#run-search").on("click", function(event) { 
       //Don't refresh the page
       event.preventDefault();
       //Code to store user input into our database
@@ -25,15 +37,13 @@
       destination = $('#destination-input').val().trim();
       firstTrainTime = $('#first-train-input').val().trim();
       frequency = $('#frequency-input').val().trim();
-      console.log(trainName);
-      console.log(destination);
-      console.log(firstTrainTime);
-      console.log(frequency);
-    //   $('.form-group').val("");
+   
+    //Clear the form after submit
       $('#train-input').val("");
       $('#destination-input').val("");
       $('#first-train-input').val("");
       $('#frequency-input').val("");
+
       //Push to database
       database.ref().push({
         trainName: trainName,
@@ -44,40 +54,51 @@
   });
   });
 
-  //Variable to reference the database
-  let database = firebase.database();
 
-  database.ref().on(
-      "value", 
-      function(snapshot) {
-      $("#train-input").append(snapshot.val().trainName);
-      $('#destination-input').append(snapshot.val().destination);
-      $('#first-train-input').append(snapshot.val().firstTrainTime);
-      $('#frequency-input').append(snapshot.val().frequency);
-      
-  }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-      
-  });
-
-//   console.log(startDate.moment().diff(moment(), 'months'));
-//   console.log(startDate.diff(newDate, "month"));
   firebase.database().ref().on("child_added", function (snapshot) {
+    //Conversion
+			//Convert to HH:MM
+			firstTimeConverted = moment(snapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+			//Converts the firsTimeCover object into string
+			
+			 // Current Time
+		  currentTime = moment();
+			diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+			
+			// Time apart (remainder)
+			tRemainder = diffTime % snapshot.val().frequency;
+
+			// Minute Until Train
+			tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+    		
+    		// Next Train
+			nextTrain = moment().add(tMinutesTillTrain, "minutes");
+			nextTrainFormat = moment(nextTrain).format('hh:mm');
+
     $("#train-table").append("<tr>");
     $("#train-table").append("<td>" + snapshot.val().trainName);
     $("#train-table").append("<td>" + snapshot.val().destination);
     $("#train-table").append("<td>" + snapshot.val().frequency);
-    $("#train-table").append("<td>" + snapshot.val().firstTrainTime + "<td>");
+    $("#train-table").append("<td>" + nextTrainFormat);
+    $("#train-table").append("<td>" + tMinutesTillTrain);
     $("#train-table").append("<hr>");
+    console.log("First Train Time: ", firstTimeConverted);
+    console.log("Current Time: ", currentTime);
+    console.log("Remainder: ", tRemainder);
+    console.log("Minutes Unitl Train", tMinutesTillTrain);
+
 })
-  database
-        .ref()
-        .orderByChild("dateAdded")
-        .limitToLast(1)
-        .on("child_added", function(snapshot) {
-          // Change the HTML to reflect
-          $("#train-input").text(snapshot.val().trainName);
-          $("#destination-input").text(snapshot.val().destination);
-          $("#first-train-input").text(snapshot.val().firstTrainTime);
-          $("#frequency-input").text(snapshot.val().frequency);
-        });
+  // database
+  //       .ref()
+  //       .orderByChild("dateAdded")
+  //       .limitToLast(1)
+  //       .on("child_added", function(snapshot) {
+  //         // Change the HTML to reflect
+  //         $("#train-input").text(snapshot.val().trainName);
+  //         $("#destination-input").text(snapshot.val().destination);
+  //         $("#first-train-input").text(snapshot.val().firstTrainTime);
+  //         $("#frequency-input").text(snapshot.val().tRemainder);
+
+  //       });
+      })
+      
